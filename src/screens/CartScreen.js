a9a409b13b40dd5b21c0c321370f8f91246c1f42
TÙNG PHAN <tungphapphap@gmail.com>
2019-12-02 00:44:25 +0700

    last update

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
-
+import { View, Text, Image, TouchableOpacity, StyleSheet, ActivityIndicator, AsyncStorage } from 'react-native';
+import global from '../global'
 
 export default class CartScreen extends React.Component {
     static navigationOptions = {
         title: 'Cart'
     }
-    render(){
-        return(
+    constructor(props) {
+        super(props);
+        this.state = {
+            cart: [],
+            user: null,
+            animating: false
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            this.setState({ animating: true })
+            const user = JSON.parse(await AsyncStorage.getItem('user'));
+            this.setState({user:user})
+            // console.log(user.username)
+            const data = await fetch(global.url + 'cart?username='+this.state.user.username);
+            const dataJson = await data.json();
+            this.setState({ cart: dataJson })
+            this.setState({ animating: false })
+            console.log(this.state.cart)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    render() {
+        return (
             <View>
                 <Text>Cart Screen</Text>
             </View>
--- src/screens/detailProduc.js
+++ src/screens/detailProduc.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {
-    View, Text, Image, SafeAreaView,
+    View, Text, Image, ToastAndroid,
     TouchableOpacity, ScrollView,
-    StyleSheet, Dimensions
+    StyleSheet, Dimensions, AsyncStorage
 } from 'react-native';
-
+import global from '../global'
+import nextId from 'react-id-generator';
 const { width, height } = Dimensions.get('window');
 export default class detailProduct extends React.Component {
     static navigationOptions = ({ navigation }) => {
@@ -12,28 +13,61 @@ export default class detailProduct extends React.Component {
             title: navigation.getParam('title', 'false')
         };
     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: '',
+            info: '',
+        }
+    }
+    
+
+
+    async addCart() {
+        try {
+            const user = JSON.parse(await AsyncStorage.getItem('user'));
+            fetch(global.url + 'cart', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: nextId(),
+                    username: user.username,
+                    productID: prodId
+                }),
+            });
+            ToastAndroid.show('Add to cart success', ToastAndroid.SHORT)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     render() {
         const info = this.props.navigation.getParam('info', 'false')
         return (
             <View style={styles.container}>
-                <ScrollView style = {{height: height}}>
-                <View style={styles.header}>
-                    <Image source={{ uri: info.image }} style={styles.image} />
-                </View>
-                <View style={styles.body}>
-                    <Text style={styles.name}>{info.name}</Text>
-                    <Text style={styles.price}>Price: {info.price}$</Text>
-                    <Text style={{margin:5}}>{info.detail}</Text>
+                <ScrollView style={{ height: height }}>
+                    <View style={styles.header}>
+                        <Image source={{ uri: info.image }} style={styles.image} />
+                    </View>
+                    <View style={styles.body}>
+                        <Text style={styles.name}>{info.name}</Text>
+                        <Text style={styles.price}>Price: {info.price}$</Text>
+                        <Text style={{ margin: 5 }}>{info.detail}</Text>
 
-                </View>
-                <View style={styles.footer}>
-                    <TouchableOpacity style={styles.btn}>
-                        <Text style={{color: '#fff',fontWeight:'bold'}}>Add To Cart</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn}>
-                        <Text style={{color: '#fff',fontWeight:'bold'}}>Pay Now</Text>
-                    </TouchableOpacity>
-                </View>
+                    </View>
+                    <View style={styles.footer}>
+                        <TouchableOpacity
+                            style={styles.btn}
+                            onPress={this.addCart}>
+                            <Text style={{ color: '#fff', fontWeight: 'bold' }}>Add To Cart</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btn}>
+                            <Text style={{ color: '#fff', fontWeight: 'bold' }}>Pay Now</Text>
+                        </TouchableOpacity>
+                    </View>
                 </ScrollView>
             </View>
         );
@@ -52,7 +86,7 @@ const styles = StyleSheet.create({
     body: {
         flex: 1,
     },
-    footer:{
+    footer: {
         flex: 1,
         marginTop: 20,
         flexDirection: "row",
@@ -60,7 +94,7 @@ const styles = StyleSheet.create({
     },
     image: {
         width: width,
-        height: height*0.5
+        height: height * 0.5
     },
     name: {
         fontSize: 30,
